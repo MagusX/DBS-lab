e239f964f6842af5512cfa03ad4023df2d7891cb
BrianDoan <brianddoan@gmail.com>
2021-05-04 00:39:44 +0700

    Add connection pool

--- src/main/java/com/bmcsdl185/lab/user/UserService.java
+++ src/main/java/com/bmcsdl185/lab/user/UserService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.List;
-
 @Service
 public class UserService {
 	Logger logger = LoggerFactory.getLogger(UserService.class);
