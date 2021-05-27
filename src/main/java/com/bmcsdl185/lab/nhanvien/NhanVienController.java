2b571ee162b1c0b4ebf0507e5b53e25e92fb9872
BrianDoan <brianddoan@gmail.com>
2021-05-27 21:45:45 +0700

    Add Staff operations

--- src/main/java/com/bmcsdl185/lab/nhanvien/NhanVienController.java
+++ src/main/java/com/bmcsdl185/lab/nhanvien/NhanVienController.java
@@ -1,6 +1,8 @@
 package com.bmcsdl185.lab.nhanvien;
 
-import com.bmcsdl185.lab.connection.PoolSerive;
+import com.bmcsdl185.lab.connection.PoolService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/staff")
 public class NhanVienController {
+	Logger logger = LoggerFactory.getLogger(NhanVienController.class);
+
 	@Autowired
 	private NhanVienService nhanVienService;
 
 	@Autowired
-	private PoolSerive poolService;
+	private PoolService poolService;
+
+	private final String redirectUrl = "redirect:/staff/%s/staff/view";
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String showLogin(Model model) {
@@ -28,14 +36,14 @@ public class NhanVienController {
 	@RequestMapping(method = RequestMethod.GET, value="/{id}")
 	public String home(@PathVariable("id") String id) {
 		if (poolService.isLoggedIn(id)) {
-			return "redirect:" + id + "/class/view/view";
+			return "redirect:" + id + "/staff/view";
 		}
 		return "staffLogin";
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/login")
 	public String login(Model model, @RequestParam("id") String id, @RequestParam("password") String password) {
-		NhanVien nhanVien = nhanVienService.getLoginInfo("SHA1", id, password);
+		NhanVien nhanVien = nhanVienService.getLoginInfo(id, password);
 
 		if (nhanVien == null) {
 			model.addAttribute("loginStatus", "failed");
@@ -43,5 +51,52 @@ public class NhanVienController {
 		}
 		poolService.newLogin(nhanVien);
 		return "redirect:" + nhanVien.getId();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value="/{staffId}/staff/{mode}")
+	public String getStaffList(Model model,
+							   @PathVariable("staffId") String staffId,
+							   @PathVariable("mode") String mode) {
+		logger.info("{}", poolService.isLoggedIn(staffId));
+		if (!poolService.isLoggedIn(staffId)) return "login";
+		List<NhanVien> staffList = nhanVienService.getStaffList();
+		model.addAttribute("mode", mode);
+		model.addAttribute("staffId", staffId);
+		model.addAttribute("staffList", staffList);
+		return "staff";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value="/{staffId}/staff/{mode}")
+	public String addStaff(@PathVariable("staffId") String staffId,
+						   @RequestParam("staffId") String id,
+						   @RequestParam("name") String name,
+						   @RequestParam("email") String email,
+						   @RequestParam("salary") int salary,
+						   @RequestParam("username") String username,
+						   @RequestParam("password") String password) {
+		if (!poolService.isLoggedIn(staffId)) return "login";
+		nhanVienService.addStaff(id, name, email, salary, username, password);
+		return String.format(redirectUrl, staffId);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/{staffId}/staff/{id}/delete")
+	public String deleteStaff(@PathVariable("staffId") String staffId,
+							  @PathVariable("id") String id) {
+		if (!poolService.isLoggedIn(staffId)) return "login";
+		nhanVienService.deleteStaff(id);
+		return String.format(redirectUrl, staffId);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/{staffId}/staff/update")
+	public String updateStaff(@PathVariable("staffId") String staffId,
+							  @RequestParam("staffId") String id,
+							  @RequestParam("name") String name,
+							  @RequestParam("email") String email,
+							  @RequestParam("salary") int salary,
+							  @RequestParam("username") String username,
+							  @RequestParam("password") String password) {
+		if (!poolService.isLoggedIn(staffId)) return "login";
+		nhanVienService.updateStaffById(id, name, email, salary, username, password);
+		return String.format(redirectUrl, staffId);
 	}
 }
--- src/main/java/com/bmcsdl185/lab/nhanvien/NhanVienService.java
+++ src/main/java/com/bmcsdl185/lab/nhanvien/NhanVienService.java
@@ -1,24 +1,111 @@
 package com.bmcsdl185.lab.nhanvien;
 
+import com.bmcsdl185.lab.encrypt.AES256;
+import com.bmcsdl185.lab.encrypt.Digest;
+import com.bmcsdl185.lab.encrypt.Utils;
 import com.bmcsdl185.lab.user.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class NhanVienService {
+	Logger logger = LoggerFactory.getLogger(NhanVienService.class);
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private AES256 aes256;
+	@Autowired
+	private Digest digest;
+	@Autowired
+	private Utils utils;
 
-	public NhanVien getLoginInfo(String algorithm, String id, String password) {
+	public NhanVien getLoginInfo(String id, String password) {
 		String sql;
+		String passwordE = "0x" + utils.toHexString(digest.hashBytes(password, "SHA1"));
 		try {
-			sql = String.format("SELECT * FROM NHANVIEN WHERE MANV = '%s' AND MATKHAU = HASHBYTES('%s',N'%s')",
-					id, algorithm, password);
+			sql = String.format("SELECT * FROM NHANVIEN WHERE MANV = '%s' AND MATKHAU = %s",
+					id, passwordE);
+			logger.info(sql);
 			return (NhanVien) jdbcTemplate.queryForObject(sql, new UserMapper());
 		} catch (EmptyResultDataAccessException e) {
 			return null;
 		}
+	}
+
+	public boolean addStaff(String id, String name, String email, int salary, String username, String password) {
+		SecretKeySpec key = aes256.getKeyIfNullCreate("18120185", "18120185", "123");
+		String salaryE;
+		String passwordE;
+
+		try {
+			salaryE = utils.toHexString(aes256.encrypt(String.valueOf(salary), key));
+			passwordE = utils.toHexString(digest.hashBytes(password, "SHA1"));
+		} catch (Exception e) {
+			logger.error("{}", e);
+			return false;
+		}
+
+		try {
+			int rows = jdbcTemplate.update(String.format("EXEC SP_INS_ENCRYPT_NHANVIEN '%s','%s','%s','%s','%s','%s'",
+					id, name, email, salaryE, username, passwordE));
+			return rows != 0;
+		} catch (Exception e) {
+			logger.warn("{}", e);
+			return false;
+		}
+	}
+
+	public List<NhanVien> getStaffList() {
+		try {
+			return jdbcTemplate.query("EXEC SP_SEL_ENCRYPT_NHANVIEN", new UserMapper())
+					.stream()
+					.map(user -> (NhanVien) user)
+					.collect(Collectors.toList());
+		} catch (EmptyResultDataAccessException e) {
+			logger.warn("{}", e);
+			return null;
+		}
+	}
+
+	public boolean updateStaffById(String id,
+								   String name,
+								   String email,
+								   int salary,
+								   String username,
+								   String password) {
+		String query = "UPDATE NHANVIEN " +
+				"SET MANV = '%s', HOTEN = N'%s', EMAIL = '%s', LUONG = convert(varbinary(max), concat('0x', '%s'), 1), TENDN = N'%s'";
+		SecretKeySpec key = aes256.getKeyIfNullCreate("18120185", "18120185", "123");
+		String salaryE;
+		String passwordE;
+
+		try {
+			salaryE = utils.toHexString(aes256.encrypt(String.valueOf(salary), key));
+			if (!password.equals("")) {
+				passwordE = utils.toHexString(digest.hashBytes(password, "SHA1"));
+				query += ", MATKHAU = convert(varbinary(max), concat('0x', '%s'), 1)";
+				query = String.format(query + " WHERE MANV = '%s'", id, name, email, salaryE, username, passwordE, id);
+			} else {
+				query = String.format(query + " WHERE MANV = '%s'", id, name, email, salaryE, username, id);
+			}
+		} catch (Exception e) {
+			logger.error("{}", e);
+			return false;
+		}
+		logger.info(query);
+		return jdbcTemplate.update(query) != 0;
+	}
+
+	public boolean deleteStaff(String id) {
+		return jdbcTemplate.update(String.format("DELETE FROM NHANVIEN WHERE MANV = '%s'", id)) != 0;
 	}
 }
