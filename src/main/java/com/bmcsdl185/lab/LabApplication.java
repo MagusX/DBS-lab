446bda47dbf428e4c0b84ce21755c8961064254a
BrianDoan <brianddoan@gmail.com>
2021-05-27 21:47:26 +0700

    Remove JDBC unnecessary call

--- src/main/java/com/bmcsdl185/lab/LabApplication.java
+++ src/main/java/com/bmcsdl185/lab/LabApplication.java
@@ -2,17 +2,9 @@ package com.bmcsdl185.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.List;
 
 @SpringBootApplication
 public class LabApplication {
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
 	public static void main(String[] args) {
 		SpringApplication.run(LabApplication.class, args);
 	}
